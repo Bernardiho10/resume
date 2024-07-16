23f479615a9a14bc0279b0596c9e3a62ba716f30
Tunde Elliot <bernardarikuoko@gmail.com>
2024-07-16 11:01:17 +0100

    Adjusted the images in my portfolio

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -13,21 +13,19 @@ const ProjectSingle = ({ title, category, image, id }) => {
       }}
     >
       <Link to={`/single-project/${id}`} aria-label="Single Project">
-        <div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
-          <div>
-            <img
-              src={image}
-              className="rounded-t-xl border-none"
-              alt="Single Project"
-            />
-          </div>
+         <div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
+          <div
+            className="rounded-t-xl flex items-center justify-center"
+            style={{ backgroundImage: `url(${image})`, height: '250px', backgroundRepeat: 'no-repeat', backgroundSize: 'contain', maxWidth: '100%', 
+              backgroundPosition: 'center'}}
+          ></div>
           <div className="text-center px-4 py-6">
             <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
               {title}
             </p>
-            <span className="text-lg text-ternary-dark dark:text-ternary-light">
+            {/* <span className="text-lg text-ternary-dark dark:text-ternary-light">
               {category}
-            </span>
+            </span> */}
           </div>
         </div>
       </Link>
