27dc7209b8bcf29721c87b9ef00d751e7f51590e
Tunde Elliot <bernardarikuoko@gmail.com>
2024-07-31 09:34:35 +0100

    Cleaned up the home page styles and positioning

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -13,7 +13,7 @@ const AppBanner = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
-      className="flex items-center justify-center mt-12 md:mt-2"
+      className="flex items-top justify-center mt-12 md:mt-2"
     >
       <div className="w-full text-center md:w-1/3">
         <motion.h1
@@ -62,7 +62,7 @@ const AppBanner = () => {
           <a
             download="Bernard-Resume-May2024.doc"
             href="/files/Bernard-Resume-May2024.doc"
-            className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+            className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-3 mb-3 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
             <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sm:w-6 sm:h-6 duration-100" />
@@ -76,9 +76,9 @@ const AppBanner = () => {
         initial={{ opacity: 0, y: -180 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
-        className="w-full sm:w-2/3 text-center mt-8 sm:mt-0"
+        className="w-full sm:w-2/3 text-center sm:mt-0"
       >
-        <img src={profileImage} className="rounded-lg mx-auto" alt="Profile" />
+        <img src={profileImage} className="rounded-lg mx-auto" style={{marginLeft: '15px'}} alt="Profile" />
       </motion.div>
     </motion.section>
   );
